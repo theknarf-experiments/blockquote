566941a3b44207ea22d58c9fe0cbc98feea3bce8
Frank Lyder Bredland <fl@nyggen.com>
2017-12-25 13:29:00 +0100

    Added connect

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,11 @@ import Editor from './editor';
 import List from './list';
 import PropTypes from 'prop-types'
 import { NewNoteAction } from '../actionreducers/notes';
+import { connect } from 'react-redux';
 
-const App = ({children}, {store}) => {
-	var editor = null;
-	var markdownDiv = null;
+const App = ({children, dispatch}) => {
+	var editor = null, markdownDiv = null;
+
 	const boldOnClick = () => {
 		if(editor !== null)
 			editor.commands.get('bold').execute();
@@ -16,76 +17,68 @@ const App = ({children}, {store}) => {
 		if(editor !== null)
 			editor.commands.get('italic').execute();
 	};
-	const newClick = () => {
-		store.dispatch(NewNoteAction());
-	}
+	const newClick = () => dispatch(NewNoteAction());
 
-	return (
-		<div className="window">
-			<header className="toolbar toolbar-header">
-				<div className="toolbar-actions" style={{ marginLeft: "80px" }}>
-				 <div className="btn-group">
-					<button className="btn btn-default">
-					  <span className="icon icon-home"></span>
-					</button>
-					<button className="btn btn-default">
-					  <span className="icon icon-folder"></span>
-					</button>
-					<button className="btn btn-default active">
-					  <span className="icon icon-cloud"></span>
-					</button>
-				 </div>
+	return <div className="window">
+		<header className="toolbar toolbar-header">
+			<div className="toolbar-actions" style={{ marginLeft: "80px" }}>
+			 <div className="btn-group">
+				<button className="btn btn-default">
+				  <span className="icon icon-home"></span>
+				</button>
+				<button className="btn btn-default">
+				  <span className="icon icon-folder"></span>
+				</button>
+				<button className="btn btn-default active">
+				  <span className="icon icon-cloud"></span>
+				</button>
+			 </div>
 
-				 <button className="btn btn-default btn-dropdown">
-					Aa	
-				 </button>
+			 <button className="btn btn-default btn-dropdown">
+				Aa	
+			 </button>
 
-				 <div className="btn-group">
-					<button className="btn btn-default" onClick={boldOnClick}><b>B</b></button>
-					<button className="btn btn-default" onClick={italicOnClick}><i>I</i></button>
-					<button className="btn btn-default underline">U</button>
-				 </div>
+			 <div className="btn-group">
+				<button className="btn btn-default" onClick={boldOnClick}><b>B</b></button>
+				<button className="btn btn-default" onClick={italicOnClick}><i>I</i></button>
+				<button className="btn btn-default underline">U</button>
+			 </div>
 
-				 <div className="btn-group">
-					<button className="btn btn-default"><span className="icon icon-code" /></button>
-					<button className="btn btn-default"><span className="icon icon-link" /></button>
-					<button className="btn btn-default"><span className="icon icon-quote" /></button>
-				 </div>
+			 <div className="btn-group">
+				<button className="btn btn-default"><span className="icon icon-code" /></button>
+				<button className="btn btn-default"><span className="icon icon-link" /></button>
+				<button className="btn btn-default"><span className="icon icon-quote" /></button>
+			 </div>
 
-				 <button className="btn btn-default btn-dropdown pull-right">
-					<span className="icon icon-megaphone"></span>
-				 </button>
-			  </div>
-			</header>
-			<div className="window-content">
-				<div className="pane-group">
-					<div className="pane-sm sidebar">
-						<List />
-					</div>
-					<div className="pane">
-						<Editor id="editor" getEditor={(e) => {
-							editor=e;
-							editor.document.on( 'changesDone', () => {
-								if(markdownDiv !== null)
-									 markdownDiv.innerHTML = editor.getData();
-							} );
-						}}><p>Editor content goes here.</p></Editor>
-						<pre ref={div => markdownDiv = div}>Markdown goes here</pre>
-					</div>
+			 <button className="btn btn-default btn-dropdown pull-right">
+				<span className="icon icon-megaphone"></span>
+			 </button>
+		  </div>
+		</header>
+		<div className="window-content">
+			<div className="pane-group">
+				<div className="pane-sm sidebar">
+					<List />
+				</div>
+				<div className="pane">
+					<Editor id="editor" getEditor={(e) => {
+						editor=e;
+						editor.document.on( 'changesDone', () => {
+							if(markdownDiv !== null)
+								 markdownDiv.innerHTML = editor.getData();
+						} );
+					}}><p>Editor content goes here.</p></Editor>
+					<pre ref={div => markdownDiv = div}>Markdown goes here</pre>
 				</div>
 			</div>
-			<footer className="toolbar toolbar-footer">
-				<div className="toolbar-actions">
-					<button className="btn btn-default" onClick={newClick}>New</button>
-					<span> Node.js {process.versions.node}, Chromeium {process.versions.chrome}, Electron {process.versions.electron} </span>
-				</div>
-			</footer>
 		</div>
-	);
+		<footer className="toolbar toolbar-footer">
+			<div className="toolbar-actions">
+				<button className="btn btn-default" onClick={newClick}>New</button>
+				<span> Node.js {process.versions.node}, Chromeium {process.versions.chrome}, Electron {process.versions.electron} </span>
+			</div>
+		</footer>
+	</div>;
 };
 
-App.contextTypes = {
-	store: PropTypes.object
-};
-
-export default App;
+export default connect(state => state)(App);
--- src/components/list.js
+++ src/components/list.js
@@ -2,35 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
 import { SelectNoteAction } from '../actionreducers/notes';
+import { connect } from 'react-redux';
 
-const List = ({}, {store}) => {
-
-	const {notes, current_note} = store.getState();
-
-	const selectNote = (id) => {
-		store.dispatch(SelectNoteAction(id));
-	};
-
-	return (
-		<ul className="list-group">
-		  <li className="list-group-header">
-			 <input className="form-control" type="text" placeholder="Search" />
-		  </li>
-		  {
-			  notes.map( note => 
-				  <li key={note.id} className={'list-group-item'+(note.id==current_note?' active':'')} onClick={() => selectNote(note.id)}>
-					 <div className="media-body">
-						<strong>List item title {note.id}</strong>
-						<p>Lorem ipsum dolor sit amet.</p>
-					 </div>
-				  </li>)
-		  }
-		</ul>
-	);
+const List = ({notes, current_note, dispatch}) => {
+	return <ul className="list-group">
+	  <li className="list-group-header">
+		 <input className="form-control" type="text" placeholder="Search" />
+	  </li>
+	  {
+		  notes.map( note => 
+			  <li
+			  		key={note.id}
+					className={'list-group-item'+(note.id==current_note?' active':'')}
+					onClick={() => dispatch(SelectNoteAction(note.id))}
+					>
+				 <div className="media-body">
+					<strong>List item title {note.id}</strong>
+					<p>Lorem ipsum dolor sit amet.</p>
+				 </div>
+			  </li>)
+	  }
+	</ul>;
 }
 
-List.contextTypes = {
-	store: PropTypes.object
-};
-
-export default List;
+export default connect(state => {
+	return {
+		notes: state.notes,
+		current_note: state.current_note
+	}
+})(List); 
